750f9fb1ecbfb6ec4730a74bf44790a5ce90e7ab
M.C. van den Boogaart <scola@kvdb.name>
2016-09-29 15:29:02 +0200

    Update dependencies and scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,18 @@
 import buble from 'rollup-plugin-buble';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default {
   entry: 'index.js',
   format: 'umd',
   globals: {
+    '@scola/d3-slider': 'd3',
     'd3-selection': 'd3',
-    '@scola/d3-slider': 'd3'
+    'd3-selection-multi': 'd3'
   },
   plugins: [
+    resolve({
+      jsnext: true
+    }),
     buble()
   ]
 };
